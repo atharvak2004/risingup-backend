af350ee8b861cc00fc912388764c2f1cbff00322
atharvak <atharvakadam631@gmail.com>
2026-04-26 14:33:09 +0530

    Refactor project structure by removing the registration app and updating settings and URLs for new app organization

--- levelup_portal/levelup_portal/urls.py
+++ levelup_portal/levelup_portal/urls.py
@@ -3,5 +3,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/", include("registration.urls")),
+
+    # New APIs (you will add as we build them)
+    # path("api/accounts/", include("accounts.urls")),
+    # path("api/school/", include("school.urls")),
+    # path("api/students/", include("students.urls")),
+    # path("api/learning/", include("learning.urls")),
+    # path("api/tests/", include("tests.urls")),
 ]
